6c23ce31f787f8fbb2edd879c6f9517194a77b76
Ruper932 <limberromero932@gmail.com>
2026-04-18 18:52:36 -0400

    login, 2FA funcionando correctamente

--- app/main.py
+++ app/main.py
@@ -13,7 +13,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins_list,
+    allow_origins=[
+        "http://localhost:3001",
+        "http://127.0.0.1:3001",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
